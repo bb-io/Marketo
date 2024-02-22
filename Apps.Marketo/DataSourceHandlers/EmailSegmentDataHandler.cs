8587b5b8cfb0c0a7274ab3169248275126686b02
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-02-22 19:55:07 +0200

    Update dynamic content logic

---
+++ Apps.Marketo/DataSourceHandlers/EmailSegmentDataHandler.cs
@@ -0,0 +1,43 @@
+﻿using Apps.Marketo.Dtos;
+using Apps.Marketo.Models.Emails.Requests;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Marketo.DataSourceHandlers
+{
+    public class EmailSegmentDataHandler : BaseInvocable, IAsyncDataSourceHandler
+    {
+        public GetEmailInfoRequest EmailInfoRequest { get; set; }
+        public GetEmailDynamicItemRequest DynamicItemRequest { get; set; }
+        public EmailSegmentDataHandler(InvocationContext invocationContext,
+            [ActionParameter] GetEmailInfoRequest emailInfoRequest, 
+            [ActionParameter] GetEmailDynamicItemRequest dynamicItemRequest) : base(invocationContext)
+        {
+            EmailInfoRequest = emailInfoRequest;
+            DynamicItemRequest = dynamicItemRequest;
+        }
+
+        public async Task<Dictionary<string, string>> GetDataAsync(DataSourceContext context,
+            CancellationToken cancellationToken)
+        {
+            if (EmailInfoRequest == null)
+                throw new ArgumentException("Please, specify an email first");
+            if (DynamicItemRequest == null)
+                throw new ArgumentException("Please, specify an email dynamic content first");
+
+            var client = new MarketoClient(InvocationContext.AuthenticationCredentialsProviders);
+            var request = new MarketoRequest($"/rest/asset/v1/email/{EmailInfoRequest.EmailId}/dynamicContent/{DynamicItemRequest.DynamicContentId}.json", Method.Get, InvocationContext.AuthenticationCredentialsProviders);
+            request.AddQueryParameter("maxReturn", 200);
+            var response = client.ExecuteWithError<DynamicContentDto>(request);
+
+            return response.Result.First().Content.Where(s => s.Type == "HTML").ToDictionary(k => k.SegmentName, v => v.SegmentName);
+        }
+    }
+}
---
+++ Apps.Marketo/Models/Emails/Requests/GetEmailDynamicItemRequest.cs
@@ -0,0 +1,18 @@
+﻿using Apps.Marketo.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Marketo.Models.Emails.Requests
+{
+    public class GetEmailDynamicItemRequest
+    {
+        [Display("Dynamic content ID")]
+        [DataSource(typeof(EmailDataHandler))]
+        public string DynamicContentId { get; set; }
+    }
+}
