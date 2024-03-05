8a84246a72ac025a6eaaa965f12dc928a97716a1
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-03-05 11:23:33 +0200

    Fix FollowupValue

--- Apps.Marketo/Dtos/FormDto.cs
+++ Apps.Marketo/Dtos/FormDto.cs
@@ -79,7 +79,7 @@ public class ThankYouListDto
     public string FollowupType { get; set; }
     
     [Display("Followup value")]
-    [JsonConverter(typeof(FollowupValueConverter))]
+    [System.Text.Json.Serialization.JsonConverter(typeof(FollowupValueConverter))]
     public object FollowupValue { get; set; }
     
     public bool Default { get; set; }
