d626462abfd086f5cd43a943a081b54ecb39cd2e
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-04-03 00:44:12 +0300

    Snippet translation actions

--- Apps.Marketo/Actions/SnippetActions.cs
+++ Apps.Marketo/Actions/SnippetActions.cs
@@ -1,20 +1,33 @@
 using Apps.Marketo.Dtos;
+using Apps.Marketo.HtmlHelpers;
 using Apps.Marketo.Invocables;
+using Apps.Marketo.Models.Emails.Requests;
+using Apps.Marketo.Models.Emails.Responses;
+using Apps.Marketo.Models.LandingPages.Requests;
+using Apps.Marketo.Models;
 using Apps.Marketo.Models.Snippets.Request;
 using Apps.Marketo.Models.Snippets.Response;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
+using System.Net.Mime;
+using System.Text;
+using System.Linq.Expressions;
 
 namespace Apps.Marketo.Actions;
 
 [ActionList]
 public class SnippetActions : MarketoInvocable
 {
-    public SnippetActions(InvocationContext invocationContext) : base(invocationContext)
+    private readonly IFileManagementClient _fileManagementClient;
+
+    public SnippetActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient) : base(invocationContext)
     {
+        _fileManagementClient = fileManagementClient;
     }
 
     [Action("Search snippets", Description = "Search snippets")]
@@ -34,8 +47,8 @@ public class SnippetActions : MarketoInvocable
         };
     }
 
-    [Action("Get snippet", Description = "Get details of a specific snippet")]
-    public SnippetDto GetSnippet([ActionParameter] SnippetRequest snippetRequest)
+    [Action("Get snippet info", Description = "Get snippet info")]
+    public SnippetDto GetSnippetInfo([ActionParameter] SnippetRequest snippetRequest)
     {
         var endpoit = $"/rest/asset/v1/snippet/{snippetRequest.SnippetId}.json";
         var request = new MarketoRequest(endpoit, Method.Get, Credentials);
@@ -82,5 +95,96 @@ public class SnippetActions : MarketoInvocable
             .AddParameter("content", input.Content);
 
         Client.ExecuteWithErrorHandling(request);
+    }
+
+    [Action("Get snippet as HTML for translation", Description = "Get snippet as HTML for translation")]
+    public async Task<FileWrapper> GetSnippetAsHtml(
+        [ActionParameter] SnippetRequest getSnippetRequest,
+        [ActionParameter] GetSegmentationRequest getSegmentationRequest,
+        [ActionParameter] GetSegmentBySegmentationRequest getSegmentBySegmentationRequest)
+    {
+        var snippetInfo = GetSnippetInfo(getSnippetRequest);
+        var snippetContentResponse = GetSnippetContent(getSnippetRequest);
+
+        if(snippetContentResponse.ContentItems.Count() == 1 && 
+           snippetContentResponse.ContentItems.First().Type == "DynamicContent")
+        {
+            snippetContentResponse.ContentItems = 
+                GetSnippetDynamicContent(getSnippetRequest, getSegmentationRequest, getSegmentBySegmentationRequest).Select(x => new SnippetContentDto(x.Type, x.Content)).ToList();
+        }
+        var sectionContent = snippetContentResponse.ContentItems!
+            .ToDictionary(
+                x => x.Type,
+                y => y.Content);
+        var resultHtml = HtmlContentBuilder.GenerateHtml(sectionContent, snippetInfo.Name, getSegmentBySegmentationRequest.Segment);
+
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(resultHtml));
+        var file = await _fileManagementClient.UploadAsync(stream, MediaTypeNames.Text.Html, $"{snippetInfo.Name}.html");
+        return new() { File = file };
+    }
+
+    [Action("Translate snippet from HTML file", Description = "Translate snippet from HTML file")]
+    public void TranslateSnippetWithHtml(
+        [ActionParameter] SnippetRequest getSnippetRequest,
+        [ActionParameter] GetSegmentationRequest getSegmentationRequest,
+        [ActionParameter] GetSegmentBySegmentationRequest getSegmentBySegmentationRequest,
+        [ActionParameter] TranslateSnippetWithHtmlRequest translateSnippetWithHtmlRequest)
+    {
+        var snippetContentResponse = GetSnippetContent(getSnippetRequest);
+        if (!(snippetContentResponse.ContentItems.Count() == 1 &&
+           snippetContentResponse.ContentItems.First().Type == "DynamicContent"))
+        {
+            ConvertSnippetToDynamicContent(getSnippetRequest.SnippetId, getSegmentationRequest.SegmentationId);
+        }
+        var snippetDynamicContent = GetSnippetDynamicContent(getSnippetRequest, getSegmentationRequest, getSegmentBySegmentationRequest);
+        var translatedContent = HtmlContentBuilder.ParseHtml(translateSnippetWithHtmlRequest.File, _fileManagementClient);
+        foreach (var item in snippetDynamicContent.Where(x => x.SegmentName == getSegmentBySegmentationRequest.Segment).ToList())
+        {
+            if ((item.Type == "HTML" || item.Type == "Text") &&
+                translatedContent.TryGetValue(item.Type, out var translatedContentItem))
+            {
+                UpdateSnippetDynamicContent(getSnippetRequest, item.SegmentId.ToString(), item.Type, translatedContentItem);
+            }
+        }
+    }
+
+    private IdDto ConvertSnippetToDynamicContent(string snippetId, string segmentationId)
+    {
+        var endpoint = $"/rest/asset/v1/snippet/{snippetId}/content.json";
+        var request = new MarketoRequest(endpoint, Method.Post, Credentials)
+            .AddParameter("content", segmentationId)
+            .AddParameter("type", "DynamicContent");
+        return Client.ExecuteWithError<IdDto>(request).Result.FirstOrDefault();
+    }
+
+    private IdDto UpdateSnippetDynamicContent(
+        SnippetRequest getSnippetRequest,
+        string segmentId,
+        string contentType,
+        string content)
+    {
+        var endpoint =
+            $"/rest/asset/v1/snippet/{getSnippetRequest.SnippetId}/dynamicContent/{segmentId}.json";
+        var request = new MarketoRequest(endpoint, Method.Post, Credentials)
+            .AddQueryParameter("type", contentType)
+            .AddQueryParameter("value", content);
+
+        return Client.GetSingleEntity<IdDto>(request);
+    }
+
+    private List<SnippetSegmentDto> GetSnippetDynamicContent(
+        SnippetRequest getSnippetRequest,
+        GetSegmentationRequest getSegmentationRequest,
+        GetSegmentBySegmentationRequest getSegmentBySegmentationRequest)
+    {
+        var requestSeg = new MarketoRequest(
+                $"/rest/asset/v1/snippet/{getSnippetRequest.SnippetId}/dynamicContent.json",
+                Method.Get, Credentials);
+        var responseSeg = Client.ExecuteWithError<SnippetDynamicContentDto>(requestSeg);
+        if (responseSeg.Result!.First().Segmentation.ToString() == getSegmentationRequest.SegmentationId)
+            return responseSeg.Result!.First().Content
+                .Where(x => x.SegmentName == getSegmentBySegmentationRequest.Segment)
+                .ToList();
+        throw new ArgumentException("Segmentation does not match!");
     }
 }
---
+++ Apps.Marketo/Dtos/SnippetSegmentDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Marketo.Dtos
+{
+    public class SnippetSegmentDto
+    {
+        public string Id { get; set; }
+        public int SegmentId { get; set; }
+        public string SegmentName { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; }
+    }
+}
