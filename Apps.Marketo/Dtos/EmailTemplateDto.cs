eac3d8fc7dd1cd8b760f6f669cb27a5a20aea2ff
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-08-14 18:32:35 +0300

    Fix datetime

--- Apps.Marketo/Dtos/EmailTemplateDto.cs
+++ Apps.Marketo/Dtos/EmailTemplateDto.cs
@@ -1,13 +1,21 @@
-﻿namespace Apps.Marketo.Dtos
+﻿using Blackbird.Applications.Sdk.Common;
+using Newtonsoft.Json;
+
+namespace Apps.Marketo.Dtos
 {
     public class EmailTemplateDto
     {
+        [Display("Created at")]
+        [JsonConverter(typeof(DateTimeConverter))]
         public DateTime CreatedAt { get; set; }
         public string Description { get; set; }
         public Folder Folder { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
         public string Status { get; set; }
+
+        [Display("Updated at")]
+        [JsonConverter(typeof(DateTimeConverter))]
         public DateTime UpdatedAt { get; set; }
         public string Url { get; set; }
         public int Version { get; set; }
