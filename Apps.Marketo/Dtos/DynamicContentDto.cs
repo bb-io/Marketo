2a792a765424a13f1a7d57b91206282cadb6c1db
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-09-23 04:04:52 +0300

    Dynamic images and alt text content

--- Apps.Marketo/Dtos/DynamicContentDto.cs
+++ Apps.Marketo/Dtos/DynamicContentDto.cs
@@ -1,11 +1,11 @@
 ﻿namespace Apps.Marketo.Dtos
 {
-    public class DynamicContentDto
+    public class DynamicContentDto<T>
     {
         public string CreatedAt { get; set; }
         public string UpdatedAt { get; set; }
         public int Id { get; set; }
         public int Segmentation { get; set; }
-        public List<EmailSegmentDto> Content { get; set; }
+        public List<T> Content { get; set; }
     }
 }
