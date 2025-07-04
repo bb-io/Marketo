7ab782ded3c57a542f8f93ad7bd5a5bee91b9d41
RiabushenkoA <ariabushenko@blackbird.io>
2025-07-04 11:45:13 +0300

    Added misconfiguration checking for inputs in action 'Get landing page as HTML for translation'

---
+++ Tests.Marketo/LandingPageTests.cs
@@ -0,0 +1,28 @@
+﻿using Apps.Marketo.Actions;
+using Apps.Marketo.Models.Emails.Requests;
+using Apps.Marketo.Models.LandingPages.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Marketo.Base;
+
+namespace Tests.Marketo
+{
+    [TestClass]
+    public class LandingPageTests :TestBase
+    {
+        [TestMethod]
+        public async Task GetLandingPages()
+        {
+            var action = new LandingPageActions(InvocationContext,FileManager);
+            var response = await action.GetLandingPageAsHtml(new GetLandingInfoRequest { Id= "1006" },
+                new GetSegmentationRequest {  SegmentationId= "1002" },
+                new GetSegmentBySegmentationRequest { Segment= "Test" },
+                new GetLandingPageAsHtmlRequest { });
+            Assert.IsNotNull(response);
+
+        }
+    }
+}
